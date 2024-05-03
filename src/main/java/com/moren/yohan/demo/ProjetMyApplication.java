9aa42c1e9b71575ad1d87104f19a0a01775d2d0b
Moren Yohan <yohan.moren@gmail.com>
2024-05-03 09:49:21 +0200

    Default config

---
+++ src/main/java/com/moren/yohan/demo/ProjetMyApplication.java
@@ -0,0 +1,13 @@
+package com.moren.yohan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moren/yohan/demo/ProjetMyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moren.yohan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetMyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
